f9de633666635aeb8902fab44c9f26d6ce9c23c9
Martin Slachta <martin.slachta@outlook.com>
2019-05-11 15:09:58 +0200

    Cleaned main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,4 @@
-#include <avg/File.h>
-#include <cstdint>
-
 // Only for testing purposes
 int main() {
-    avg::File::FileWatcher file = avg::File::FileWatcher("/home/martin/Projects/avgLib/test.txt");
     return 0;
 }
