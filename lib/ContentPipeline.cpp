4f39159c762ea6d5a371d947a534687e8a10d963
Aproximate <martin.slachta@outlook.com>
2019-07-16 10:58:46 +0200

    Debug into reading file

--- lib/ContentPipeline.cpp
+++ lib/ContentPipeline.cpp
@@ -5,6 +5,12 @@
 
 char* avg::ContentPipeline::loadText(const char* path) {
     std::ifstream stream = std::ifstream(path, std::ios::ate);
+
+    #if DEBUG
+    if(!stream.is_open())
+        std::cout << "Couldn't open file." << std::endl;
+    #endif
+
     uint32_t size = stream.tellg();
 
     stream.seekg(0);
